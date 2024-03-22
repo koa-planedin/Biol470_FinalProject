08fc728ec6895438871bab82ca466569c9968b41
Larissa Bron <larissabron@gmail.com>
2024-03-22 09:27:42 -0700

    Heck ya

--- project_script.sh
+++ project_script.sh
@@ -17,5 +17,21 @@ chmod ug+rwx koa_ola_lars/
 # Copy project files from Greg's directory
 cp -r  /project/ctb-grego/sharing/biol470_project /project/ctb-grego/biol470/koa_ola_lars
 
-# What a good time to be alive
+# Talk to the group about how you want to work together on code. Right now you have set up a github repository and it has been working alright to install git on your computer, as well as the GitHub desktop and Visual Code. Does Koa have a better work around? 
+
+# Install git
+https://git-scm.com/book/en/v2/Getting-Started-Installing-Git
+
+# Set up git basics
+$ git config --global user.name "Human Name"
+$ git config --global user.email humanname@example.com
+
+# Install GitHub desktop for pushing updates to code
+https://desktop.github.com/
+
+# Install visual code to have a nice environment to work on your scripts within
+https://code.visualstudio.com/
+
+# Clone the repository for our project
+https://github.com/larissaissabron/Biol470_FinalProject
 
